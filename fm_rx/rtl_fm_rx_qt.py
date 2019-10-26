929fddeee97cd112f07e638e05391be27c4ce62d
mkhuthir <mkhuthir@gmail.com>
2019-10-26 11:14:24 +0400

    updated fm rx

---
+++ fm_rx/rtl_fm_rx_qt.py
@@ -0,0 +1,225 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+##################################################
+# GNU Radio Python Flow Graph
+# Title: FM Receiver
+# Author: Muthanna A. Alwahash
+# Generated: Fri Oct 25 06:22:38 2019
+##################################################
+
+from distutils.version import StrictVersion
+
+if __name__ == '__main__':
+    import ctypes
+    import sys
+    if sys.platform.startswith('linux'):
+        try:
+            x11 = ctypes.cdll.LoadLibrary('libX11.so')
+            x11.XInitThreads()
+        except:
+            print "Warning: failed to XInitThreads()"
+
+from PyQt5 import Qt
+from PyQt5 import Qt, QtCore
+from gnuradio import analog
+from gnuradio import audio
+from gnuradio import blocks
+from gnuradio import eng_notation
+from gnuradio import filter
+from gnuradio import gr
+from gnuradio import qtgui
+from gnuradio.eng_option import eng_option
+from gnuradio.filter import firdes
+from gnuradio.qtgui import Range, RangeWidget
+from optparse import OptionParser
+import osmosdr
+import sip
+import sys
+import time
+from gnuradio import qtgui
+
+
+class rtl_fm_rx_qt(gr.top_block, Qt.QWidget):
+
+    def __init__(self):
+        gr.top_block.__init__(self, "FM Receiver")
+        Qt.QWidget.__init__(self)
+        self.setWindowTitle("FM Receiver")
+        qtgui.util.check_set_qss()
+        try:
+            self.setWindowIcon(Qt.QIcon.fromTheme('gnuradio-grc'))
+        except:
+            pass
+        self.top_scroll_layout = Qt.QVBoxLayout()
+        self.setLayout(self.top_scroll_layout)
+        self.top_scroll = Qt.QScrollArea()
+        self.top_scroll.setFrameStyle(Qt.QFrame.NoFrame)
+        self.top_scroll_layout.addWidget(self.top_scroll)
+        self.top_scroll.setWidgetResizable(True)
+        self.top_widget = Qt.QWidget()
+        self.top_scroll.setWidget(self.top_widget)
+        self.top_layout = Qt.QVBoxLayout(self.top_widget)
+        self.top_grid_layout = Qt.QGridLayout()
+        self.top_layout.addLayout(self.top_grid_layout)
+
+        self.settings = Qt.QSettings("GNU Radio", "rtl_fm_rx_qt")
+        self.restoreGeometry(self.settings.value("geometry", type=QtCore.QByteArray))
+
+
+        ##################################################
+        # Variables
+        ##################################################
+        self.volume = volume = 0.1
+        self.transition = transition = 1e6
+        self.samp_rate = samp_rate = 2e6
+        self.quadrature = quadrature = 500e3
+        self.cutoff = cutoff = 100e3
+        self.audio_dec = audio_dec = 10
+        self.FM_Frequency = FM_Frequency = 94.4e6
+
+        ##################################################
+        # Blocks
+        ##################################################
+        self._volume_range = Range(0.1, 1, 0.01, 0.1, 200)
+        self._volume_win = RangeWidget(self._volume_range, self.set_volume, 'Volume', "slider", float)
+        self.top_grid_layout.addWidget(self._volume_win)
+        self._FM_Frequency_range = Range(88e6, 108e6, 100e3, 94.4e6, 200)
+        self._FM_Frequency_win = RangeWidget(self._FM_Frequency_range, self.set_FM_Frequency, 'FM Channel Frequency', "counter_slider", float)
+        self.top_grid_layout.addWidget(self._FM_Frequency_win)
+        self.rtlsdr_source_0 = osmosdr.source( args="numchan=" + str(1) + " " + '' )
+        self.rtlsdr_source_0.set_sample_rate(2e6)
+        self.rtlsdr_source_0.set_center_freq(FM_Frequency, 0)
+        self.rtlsdr_source_0.set_freq_corr(0, 0)
+        self.rtlsdr_source_0.set_dc_offset_mode(0, 0)
+        self.rtlsdr_source_0.set_iq_balance_mode(0, 0)
+        self.rtlsdr_source_0.set_gain_mode(False, 0)
+        self.rtlsdr_source_0.set_gain(10, 0)
+        self.rtlsdr_source_0.set_if_gain(20, 0)
+        self.rtlsdr_source_0.set_bb_gain(20, 0)
+        self.rtlsdr_source_0.set_antenna('', 0)
+        self.rtlsdr_source_0.set_bandwidth(0, 0)
+
+        self.rational_resampler_xxx_1 = filter.rational_resampler_ccc(
+                interpolation=1,
+                decimation=int(samp_rate/quadrature),
+                taps=None,
+                fractional_bw=None,
+        )
+        self.rational_resampler_xxx_0 = filter.rational_resampler_fff(
+                interpolation=48,
+                decimation=int(quadrature/1e3/audio_dec),
+                taps=None,
+                fractional_bw=None,
+        )
+        self.qtgui_sink_x_0 = qtgui.sink_c(
+        	1024, #fftsize
+        	firdes.WIN_BLACKMAN_hARRIS, #wintype
+        	0, #fc
+        	samp_rate, #bw
+        	"", #name
+        	True, #plotfreq
+        	True, #plotwaterfall
+        	True, #plottime
+        	True, #plotconst
+        )
+        self.qtgui_sink_x_0.set_update_time(1.0/10)
+        self._qtgui_sink_x_0_win = sip.wrapinstance(self.qtgui_sink_x_0.pyqwidget(), Qt.QWidget)
+        self.top_grid_layout.addWidget(self._qtgui_sink_x_0_win)
+
+        self.qtgui_sink_x_0.enable_rf_freq(False)
+
+
+
+        self.low_pass_filter = filter.fir_filter_ccf(1, firdes.low_pass(
+        	1, samp_rate, cutoff, transition, firdes.WIN_HAMMING, 6.76))
+        self.blocks_multiply_const_vxx_0 = blocks.multiply_const_vff((volume, ))
+        self.audio_sink_0 = audio.sink(48000, '', True)
+        self.analog_wfm_rcv = analog.wfm_rcv(
+        	quad_rate=quadrature,
+        	audio_decimation=audio_dec,
+        )
+
+
+
+        ##################################################
+        # Connections
+        ##################################################
+        self.connect((self.analog_wfm_rcv, 0), (self.rational_resampler_xxx_0, 0))
+        self.connect((self.blocks_multiply_const_vxx_0, 0), (self.audio_sink_0, 0))
+        self.connect((self.low_pass_filter, 0), (self.analog_wfm_rcv, 0))
+        self.connect((self.rational_resampler_xxx_0, 0), (self.blocks_multiply_const_vxx_0, 0))
+        self.connect((self.rational_resampler_xxx_1, 0), (self.low_pass_filter, 0))
+        self.connect((self.rtlsdr_source_0, 0), (self.qtgui_sink_x_0, 0))
+        self.connect((self.rtlsdr_source_0, 0), (self.rational_resampler_xxx_1, 0))
+
+    def closeEvent(self, event):
+        self.settings = Qt.QSettings("GNU Radio", "rtl_fm_rx_qt")
+        self.settings.setValue("geometry", self.saveGeometry())
+        event.accept()
+
+    def get_volume(self):
+        return self.volume
+
+    def set_volume(self, volume):
+        self.volume = volume
+        self.blocks_multiply_const_vxx_0.set_k((self.volume, ))
+
+    def get_transition(self):
+        return self.transition
+
+    def set_transition(self, transition):
+        self.transition = transition
+        self.low_pass_filter.set_taps(firdes.low_pass(1, self.samp_rate, self.cutoff, self.transition, firdes.WIN_HAMMING, 6.76))
+
+    def get_samp_rate(self):
+        return self.samp_rate
+
+    def set_samp_rate(self, samp_rate):
+        self.samp_rate = samp_rate
+        self.qtgui_sink_x_0.set_frequency_range(0, self.samp_rate)
+        self.low_pass_filter.set_taps(firdes.low_pass(1, self.samp_rate, self.cutoff, self.transition, firdes.WIN_HAMMING, 6.76))
+
+    def get_quadrature(self):
+        return self.quadrature
+
+    def set_quadrature(self, quadrature):
+        self.quadrature = quadrature
+
+    def get_cutoff(self):
+        return self.cutoff
+
+    def set_cutoff(self, cutoff):
+        self.cutoff = cutoff
+        self.low_pass_filter.set_taps(firdes.low_pass(1, self.samp_rate, self.cutoff, self.transition, firdes.WIN_HAMMING, 6.76))
+
+    def get_audio_dec(self):
+        return self.audio_dec
+
+    def set_audio_dec(self, audio_dec):
+        self.audio_dec = audio_dec
+
+    def get_FM_Frequency(self):
+        return self.FM_Frequency
+
+    def set_FM_Frequency(self, FM_Frequency):
+        self.FM_Frequency = FM_Frequency
+        self.rtlsdr_source_0.set_center_freq(self.FM_Frequency, 0)
+
+
+def main(top_block_cls=rtl_fm_rx_qt, options=None):
+
+    qapp = Qt.QApplication(sys.argv)
+
+    tb = top_block_cls()
+    tb.start()
+    tb.show()
+
+    def quitting():
+        tb.stop()
+        tb.wait()
+    qapp.aboutToQuit.connect(quitting)
+    qapp.exec_()
+
+
+if __name__ == '__main__':
+    main()
