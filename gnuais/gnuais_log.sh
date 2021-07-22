5499cd5554a9b85af87455ec942a69178833e185
Muthanna <mkhuthir@gmail.com>
2021-07-22 09:04:12 +0400

    ais

--- gnuais/gnuais_log.sh
+++ gnuais/gnuais_log.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 echo "Writing decoded messages to $(date +"%Y_%m_%d_%I_%M_%p").log"
-rtl_ais -n  2> "$(date +"%Y_%m_%d_%I_%M_%p").log"
+gnuais -o none >"$(date +"%Y_%m_%d_%I_%M_%p").log"
